8f3ba72168dc9d7074fe03171d5c3b6e4bdf35e9
Connor Midgley <53975931+GameMagma@users.noreply.github.com>
2023-08-02 17:45:32 -0500

    Structure

---
+++ main.py
@@ -0,0 +1,25 @@
+def findWord(blanks: int, nums: list):
+    """
+    This method uses the blanks and numbers provided to guess what the word could be
+
+    :param blanks: The number of blanks for the word
+    :param nums: The numbers associated with the blanks
+    :return: The word
+    """
+    pass
+
+
+def findSolution(blanks, nums):
+    """
+    This method uses the list of words that have already been solved and the numbers that have been found using the
+    previous methods to find the final solution.
+
+    :param blanks: A list of each group of blanks, consisting of a number of blanks for the solution to the puzzle
+    :param nums: The numbers associated with the blanks
+    :return: The solution
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
